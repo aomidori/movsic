7b2ddfced2368e233ec7f357c343e180771e84cb
Hanyue Zhou <alexalexchou@outlook.com>
2017-11-19 21:56:55 +0100

    facebook login dekita! QAQ

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 import { SignupPage } from '../signup/signup';
 import { TabsPage } from '../tabs/tabs';
@@ -9,7 +10,7 @@ import { User } from '../../models/user';
 
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
   user = {} as User;  // an objecct as User
@@ -28,6 +29,23 @@ export class LoginPage {
       }
     }catch(e){
       console.error(e);
+    }
+  }
+
+  async loginWithFacebook(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_birthday');
+    provider.addScope('email');
+
+    const result = await firebase.auth().signInWithPopup(provider).then(function(result){
+        var token = result.credential.accessToken;
+        var user = result.user;
+        return true;
+    }).catch(function(e){
+        console.error(e);
+    });
+    if(result){
+      this.nav.setRoot(TabsPage);
     }
   }
 }
