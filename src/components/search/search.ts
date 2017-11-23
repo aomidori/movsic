889dc2731c4ce9306f21b9e1a2ee1d4df681b6f9
Hanyue Zhou <alexalexchou@outlook.com>
2017-11-23 22:20:19 +0100

    visualize the search result

--- src/components/search/search.ts
+++ src/components/search/search.ts
@@ -1,22 +1,63 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { SpotifyServiceProvider } from '../../providers/spotify-service/spotify-service';
+import { Album } from '../../models/album';
 
-/**
- * Generated class for the SearchComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'search',
-  templateUrl: 'search.html'
+  templateUrl: 'search.html',
+  providers: [SpotifyServiceProvider]
 })
 export class SearchComponent {
 
-  text: string;
+  query: string;
+  token: string;
+  searchRes: string;
+  dropdownDisplay: boolean;
+  albums: Album[];
 
-  constructor() {
-    console.log('Hello SearchComponent Component');
-    this.text = 'Hello World';
+  constructor(
+    public nav: NavController,
+    private _spotifyService: SpotifyServiceProvider
+  ) {
+    this.dropdownDisplay = false;
+    this.albums = [];
+  }
+
+  searchMusic(){
+    this.albums = [];
+    const trimmed_query = this.query.trim();
+    if(!trimmed_query || trimmed_query.length ==0 ){
+      this.dropdownDisplay=false;
+      return;
+    }
+    this._spotifyService.getToken()
+         .subscribe(res => {
+             this._spotifyService.searchMusic(trimmed_query,'album' , res.access_token)
+               .subscribe(res=> {
+                 this.searchRes = res.albums.items;
+                 for (let item of res.albums.items){
+                  let images = item.images;
+                  let album: Album={
+                    spotify_id: item.id,
+                    img_url: images[2].url,
+                    name: item.name
+                  };
+                  this.albums.push(album);
+                }
+            })
+         })
+    if (this.searchRes){
+      this.dropdownDisplay = true;
+    }
+    else{
+      this.dropdownDisplay=false;
+    }
+    console.log(this.albums);
+    //this.searchResult.type =
+
+
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
+//import { Observable } from 'rxjs/Observable';
+//import { SearchDropdownComponent } from '../../components/search-dropdown/search-dropdown';
+//import 'rxjs/Rx';
 import { SpotifyServiceProvider } from '../../providers/spotify-service/spotify-service';
-
+import { SearchComponent } from '../../components/search/search';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
   providers:[SpotifyServiceProvider]
 })
 export class HomePage {
-  searchStr: string;
+  /*searchStr: string;
   token: string;
-  searchRes: string;
+  searchRes: string;*/
   constructor(public navCtrl: NavController, public http:Http, private _spotifyService:SpotifyServiceProvider) {
 
   }
-  searchMusic(){
+  /*searchMusic(){
     this._spotifyService.getToken()
          .subscribe(res => {
              this._spotifyService.searchMusic(this.searchStr ,'album' , res.access_token)
@@ -26,6 +28,6 @@ export class HomePage {
             })
          })
          console.log(this.searchRes);
-  }
+  }*/
 
 }
