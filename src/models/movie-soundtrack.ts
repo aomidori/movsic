1b2da2e3b2172e01832cd73874c5b0189575b649
Hanyue Zhou <alexalexchou@outlook.com>
2017-11-24 04:51:56 +0100

    object type to any

--- src/models/movie-soundtrack.ts
+++ src/models/movie-soundtrack.ts
@@ -2,7 +2,7 @@ import { Artist } from './artist';
 import { MovieInfo } from './movie-info';
 
 export interface MovieSoundtrack{
-  movie_info: MovieInfo;
+  movie_info: any;
   spotify_id: string;
   img_url: string;
   name: string;
